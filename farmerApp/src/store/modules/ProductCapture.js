e0b0215e4132d11b44378676820c70ecdeacf524
Dylan <dylan@vandenbosch.co.za>
2018-10-19 19:52:34 +0200

    Product Capture begins

---
+++ farmerApp/src/store/modules/ProductCapture.js
@@ -0,0 +1,19 @@
+const state = {
+    product: null
+}
+
+const getters = {
+    product(state) {
+        return state.product
+    }
+}
+
+const actions = {
+
+}
+
+export default {
+    state,
+    getters,
+    actions
+}
--- farmerApp/src/views/ProductCapture.vue
+++ farmerApp/src/views/ProductCapture.vue
@@ -1,6 +1,16 @@
 <template>
     <v-container grid-list-xs>
         <h3 class="display-1">Products</h3>
+        <v-text-field
+            hint persistent-hint
+            label="label"
+            loading="false"
+            mask="mask"
+        ></v-text-field>
+        <v-text-field
+            label="Type of Product"
+            id="id"
+        ></v-text-field>
         
     </v-container>
 </template>
